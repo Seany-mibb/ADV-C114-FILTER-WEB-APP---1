4143a3de317160a3157124f11f2d92a5270257d1
Seany-mibb <86206802+Seany-mibb@users.noreply.github.com>
2022-06-29 20:19:04 -0700

    Add files via upload

---
+++ mustache.js
@@ -0,0 +1,36 @@
+function setup()
+{
+    canvas = createCanvas(300, 300);
+    canvas.position(windowWidth/2 - 150, windowHeight/2-130);
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
+}
+
+function take_snapshot()
+{
+    save('MustacheFilter.png');
+}
+
+function draw()
+{
+    image(video,0,0,300,300);
+}
+
+function modelLoaded()
+{
+    console.log("Huzzah model is loaded");
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+    }
+}
